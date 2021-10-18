86fb18b17b4b75e29862b6869ce76091e19c1c0b
rberneckuu <92716844+rberneckuu@users.noreply.github.com>
2021-10-18 12:12:48 +0200

    chore(pipfile): install matplotlib

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from points import Points
+import matplotlib.pyplot as plt
 
 def main():
     """ Main program """
+
+    
+
     points = Points(10, 10)
+    
+    plt.plot([1, 2, 3, 4])
+    plt.ylabel('some numbers')
+    plt.show()
     return 0
 
 if __name__ == "__main__":
