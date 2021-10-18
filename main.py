18c99b967ce8a03aa39614cd68295fd15124cd61
rberneckuu <92716844+rberneckuu@users.noreply.github.com>
2021-10-18 13:17:46 +0200

    feat(main): plot triangle

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from points import Points
 import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
-    """ Main program """
-
-    
+    """ Main program """ 
 
     points = Points(10, 10)
-    
-    plt.plot([1, 2, 3, 4])
-    plt.ylabel('some numbers')
+    x = [point[0] for point in points]
+    y = [point[1] for point in points]
+
+    tri_coordinates = np.array([[3,3], [7,3], [7, 7]])
+    triangle = plt.Polygon(tri_coordinates, edgecolor="black", facecolor="None")
+    plt.gca().add_patch(triangle)
+
+    plt.scatter(x, y)
+    plt.axis([0, 10, 0, 10])
+    plt.title("k-NN classification")
+  
+
     plt.show()
     return 0
 
 if __name__ == "__main__":
     main()
-
-
