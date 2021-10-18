1cd128b142da600bd9a9fb122b26376fafdc1f11
Robert Oost <robertwilliamoost@gmail.com>
2021-10-18 23:18:37 +0200

    feat(main): plot everything easily

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
-from points import Points
+from point_distribution import PointDistribution
+
+from triangle import Triangle
+from plotter import ClassificationPlotter
 import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
     """ Main program """ 
 
-    points = Points(10, 10)
-    x = [point[0] for point in points]
-    y = [point[1] for point in points]
-
-    tri_coordinates = np.array([[3,3], [7,3], [7, 7]])
-    triangle = plt.Polygon(tri_coordinates, edgecolor="black", facecolor="None")
-    plt.gca().add_patch(triangle)
-
-    plt.scatter(x, y)
-    plt.axis([0, 10, 0, 10])
-    plt.title("k-NN classification")
-  
-
-    plt.show()
+    points = PointDistribution(10, 100, 0)
+    triangle = Triangle([[3,3], [7,3], [7,7]])
+    
+    plotter = ClassificationPlotter(points, triangle)
+    plotter.show()
     return 0
 
 if __name__ == "__main__":
