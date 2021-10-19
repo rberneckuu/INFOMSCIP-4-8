4f971d6fd4fbe00cb7490a5993ffd58134669d68
Robert Oost <robertwilliamoost@gmail.com>
2021-10-19 12:08:22 +0200

    feat(main): fit knn classifier

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ from plotter import ClassificationPlotter
 import matplotlib.pyplot as plt
 import numpy as np
 
+from sklearn.neighbors import KNeighborsClassifier
+
 def main():
     """ Main program """ 
 
     points = PointDistribution(10, 100, 0.01)
     triangle = Triangle([[3,3], [7,3], [7,7]])
+    labels = points.get_labels(triangle)
     
-    plotter = ClassificationPlotter(points, triangle)
+    plotter = ClassificationPlotter(points, triangle, labels)
+    
+    knn = KNeighborsClassifier(n_neighbors=3)
+    knn.fit(points, labels)
+    
     plotter.show()
     return 0
 
