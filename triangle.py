65d2d4d2b1b8a1926ab2c9462e33e01910c554c1
Robert Oost <robertwilliamoost@gmail.com>
2021-10-19 02:07:42 +0200

    fix(triangle): return value

--- triangle.py
+++ triangle.py
@@ -11,7 +11,7 @@ class Triangle:
         d3 = self.__sign_point(scatter_point, v3, v1)
         has_neg = (d1 < 0) or (d2 < 0) or (d3 < 0)
         has_pos = (d1 > 0) or (d2 > 0) or (d3 > 0)
-        print(not(has_neg and has_pos))
+        return (not(has_neg and has_pos))
 
     def __sign_point(self, p1, p2, p3):
         return (p1[0] - p3[0]) * (p2[1] - p3[1]) - (p2[0] - p3[0]) * (p1[1] - p3[1])
