4633c3f58f0c0fb4d0e1f3c651e83e8c199bacda
Robert Oost <robertwilliamoost@gmail.com>
2021-10-22 13:14:09 +0200

    chore(analysis): remove unused code

--- analysis.py
+++ analysis.py
@@ -1,11 +1,10 @@
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-def get_scores(data):
-    for idx, experiment in enumerate(data):
-        y_pred, y_true = experiment
+def get_scores(y_pred, y_true):
 
-        print("\nPart ", idx + 1)
-        print("accuracy score: ", accuracy_score(y_true, y_pred))
-        print("precision score: ", precision_score(y_true, y_pred))
-        print("recall score: ", recall_score(y_true, y_pred))
-        print("f1-score: ", f1_score(y_true, y_pred))
+    accuracy = accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred)
+
+    return (accuracy, precision, recall, f1)
