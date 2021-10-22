ee2b90390625baf044607049e95ba3f97d281767
Robert Oost <robertwilliamoost@gmail.com>
2021-10-22 13:17:55 +0200

    feat(main): do experiments, export to excel

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 from experiments import experiment_1, experiment_2, experiment_3
-from analysis import get_scores
+
+import pandas as pd
 
 def main():
     """ Main program """
+    writer = pd.ExcelWriter(f".\exports\experiments.xlsx")
+
+    scores1 = experiment_1()
+    scores2 = experiment_2()
+    scores3 = experiment_3()
+
+    scores1.to_excel(sheet_name="Experiment 1", excel_writer=writer)
+    scores2.to_excel(sheet_name="Experiment 2", excel_writer=writer)
+    scores3.to_excel(sheet_name="Experiment 3", excel_writer=writer)
+    writer.close()
     
-    print("\n######## RUNNING EXPERIMENT 1")
-    exp1_data = experiment_1()
-    get_scores(exp1_data)
-
-    print("\n######## RUNNING EXPERIMENT 2")
-    exp2_data = experiment_2()
-    get_scores(exp2_data)
-
-    print("\n######## RUNNING EXPERIMENT 3")
-    exp3_data = experiment_3()
-    get_scores(exp3_data)
-
     return 0
 
 if __name__ == "__main__":
