e14830c74a7abc17aa0e0039a872352534dfa252
Robert Oost <robertwilliamoost@gmail.com>
2021-10-24 03:23:22 +0200

    feat(plotter): plot triangles with labels

--- plotter.py
+++ plotter.py
@@ -19,7 +19,8 @@ class ClassificationPlotter:
         plt.show()
 
     def __plot_points(self):
-        x, y = self.__distribution.get_x_y()
+        x = [point[0] for point in self.__distribution]
+        y = [point[1] for point in self.__distribution]
         
         colormap = cm.get_cmap('coolwarm')
         plt.scatter(x, y, c=self.__labels, cmap=colormap)
@@ -27,6 +28,46 @@ class ClassificationPlotter:
 
     def __plot_triangle(self):
 
-        points = self.__triangle.points
+        points = self.__triangle
         triangle = plt.Polygon(points, edgecolor="black", facecolor="None")
         plt.gca().add_patch(triangle)
+
+    @classmethod
+    def plot_triangles(cls, triangles):
+        
+        # Prepare vertex labels.
+        point_names = ['A', 'B', 'C']
+        point_offsets = [[-0.5, -0.75],[-0.5, -0.75],[0, 0.35]]
+
+        for idx, triangle in enumerate(triangles):
+            tri_number = idx + 1
+
+            for idy, point in enumerate(triangle):
+                x, y = point
+
+                # Get text position
+                offset_x, offset_y = point_offsets[idy]
+                text_x, text_y = (x + offset_x, y + offset_y)
+                
+                # Add text to the plot for the triangle point.
+                point_name = point_names[idy]
+                plt.text(text_x, text_y, f"{point_name}: ({x}, {y})")
+            
+            # Add the triangle to the plot
+            poly = plt.Polygon(triangle, edgecolor="black", facecolor="None")
+            plt.gca().add_patch(poly)
+
+            plt.suptitle(f"Triangle {tri_number}")
+
+            # Calculate perimeter for the subtitle.
+            perimeter = triangle.get_perimeter()
+            perimeter = round(perimeter, 2)
+            plt.title(f"Perimeter ≈ {perimeter}", fontsize=10)
+
+            # Set the size of the plot, then hide the axes.
+            plt.axis([0, 10, 0, 10])
+            plt.axis('off')
+
+            # Save and show the plot.
+            plt.savefig(f"./exports/triangle-{tri_number}.png", bbox_inches="tight")
+            plt.show()
